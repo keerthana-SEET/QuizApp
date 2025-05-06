0a5612af5dcb94e75da9ed80f6a5737f90d1f4ea
keerthana-SEET <keerthana.mettapalli@serialxtech.com>
2025-05-06 11:15:11 +0530

    chnages

--- resources/js/components/ui/sidebar/SidebarProvider.vue
+++ resources/js/components/ui/sidebar/SidebarProvider.vue
@@ -51,7 +51,7 @@ useEventListener('keydown', (event: KeyboardEvent) => {
 
 // We add a state so that we can do data-state="expanded" or "collapsed".
 // This makes it easier to style the sidebar with Tailwind classes.
-const state = computed(() => open.value ? 'collapsed' : 'collapsed')
+const state = computed(() => open.value ? 'expanded' : 'collapsed')
 
 provideSidebarContext({
   state,
